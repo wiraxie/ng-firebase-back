e9334fb61b6db41662ded4c6ea98830243729ea0
WiraXie <wiraxie.wira_xie@ymail.com>
2017-11-30 09:49:20 +0700

    30nov2017

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -83,7 +83,7 @@ export class AuthService implements OnInit
     return this.afAuth.auth.createUserWithEmailAndPassword(email, password)
       .then((user) => 
       {
-        this.authState = new user
+        this.authState
       })
       .catch(error => 
       {
